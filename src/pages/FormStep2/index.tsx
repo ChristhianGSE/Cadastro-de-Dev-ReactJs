6bde31e9b1a18f7b3dfe5a5f2de01e770d9af7a9
ChristhianEspindola <espindolachristhian@gmail.com>
2022-02-07 19:06:31 -0300

    commit

---
+++ src/pages/FormStep2/index.tsx
@@ -0,0 +1,69 @@
+import { useNavigate, Link } from 'react-router-dom';
+import * as C from './styles';
+import { Theme } from '../../components/Theme';
+import { useForm, FormActions } from '../../contexts/FormContext';
+import { ChangeEvent, useEffect } from 'react';
+import { SelectOption } from '../../components/SelectOption';
+
+export const FormStep2 = () => {
+    const history = useNavigate();
+    const { state, dispatch } = useForm();
+
+    useEffect(() => {
+        if(state.name === '') {
+            history("/");
+        }else {
+            dispatch({
+                type: FormActions.setCurrentStep,
+                payload: 2
+            });
+        }  
+    }, []);
+
+    const handleNextStep = () => {
+        if(state.name !== '') {
+            history("/step3");
+        } else {
+            alert("Preencha os dados.");
+        }
+        
+    }
+
+    const setLevel = (level: number) => {
+        dispatch({
+            type: FormActions.SetLevel,
+            payload: level
+        });
+    }
+
+    return (
+       <Theme>
+           <C.Container>
+               <p>Passo 2/3</p>
+               <h1>{state.name}, o que melhor descreve você?</h1>
+               <p>Escolha a opção que melhor condiz com seu estado atual, profisionamente.</p>
+
+               <hr/>
+
+                <SelectOption 
+                    title="Sou iniciante"
+                    description="Comecei a programar há menos de 2 anos"
+                    icon="😁"
+                    selected={state.level === 0}
+                    onClick={()=>setLevel(0)}
+                />
+
+                <SelectOption 
+                    title="Sou programador"
+                    description="Já programo há 2 anos ou mais"
+                    icon="😎"
+                    selected={state.level === 1}
+                    onClick={()=>setLevel(1)}
+                />
+
+                <Link to="/" className='backButton'>Voltar</Link>
+                <button onClick={handleNextStep}>Próximo</button>
+           </C.Container>
+       </Theme>
+    )
+}
